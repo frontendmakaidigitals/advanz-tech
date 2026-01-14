f3163c6ab11e95eed4266900c379bfc7e75ad235
Faheem <faheemk793@gmail.com>
2026-01-14 17:23:09 +0530

    fixed header and about

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 import Navbtn from "./navbtn";
 const Header = () => {
   return (
-    <div className=" fixed z-10 top-0 left-0 w-full bg-black/50 backdrop-blur-2xl  py-3 ">
+    <div className=" fixed z-10 top-0 left-0 w-full bg-white/50 backdrop-blur-2xl  py-3 ">
       <div className="max-w-6xl flex justify-between mx-auto items-center">
         <Logo className={""} />
         <NavigationMenuDemo />
